13d0b0b40614a09542d5d877137524ca1836a8af
anujedits66-lang <anujedits66@gmail.com>
2026-03-02 08:58:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+FROM python:3.8-slim-buster
+
+RUN apt-get update && apt-get install -y git && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /EvaMaria
+
+COPY requirements.txt .
+RUN pip3 install --upgrade pip
+RUN pip3 install -r requirements.txt
+
+COPY . .
+RUN chmod +x start.sh
+
+CMD ["./start.sh"]
